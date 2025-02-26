abb570d39d768fcef168d28b4f05c09aa1544253
ahmed <ahmed.abdelkader.khedr@gmail.com>
2025-02-26 10:30:00 +0200

    Ver 2.0

--- src/routes/user.js
+++ src/routes/user.js
@@ -56,7 +56,7 @@ router.get('/login/google/callback',
       
     // });
 
-    res.redirect(`myapp://login?token=${token}&refreshToken=${refreshToken}`);
+    res.redirect(`myapp://login?id=${id}token=${token}&refreshToken=${refreshToken}`);
 
   }
 );

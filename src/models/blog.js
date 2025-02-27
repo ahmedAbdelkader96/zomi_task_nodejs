a19ea88c7ad3fbbe663d0d22434ba58af26c4c94
ahmed <ahmed.abdelkader.khedr@gmail.com>
2025-02-27 18:12:24 +0200

    Ver1.9

--- src/models/blog.js
+++ src/models/blog.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const blogSchema = mongoose.Schema({
     _id: { type: String, required: false },
     title: { type: String, required: true },
-    describtion: { type: String, required: true },
+    description: { type: String, required: true },
     date: { type: String, required: true }, 
     image: { type: String, required: true },
 },

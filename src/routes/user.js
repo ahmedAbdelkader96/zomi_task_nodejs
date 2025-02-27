5238c2c8e99a3911d8af41ffac9368b2e9841a27
ahmed <ahmed.abdelkader.khedr@gmail.com>
2025-02-27 09:02:21 +0200

    Ver1.3

--- src/routes/user.js
+++ src/routes/user.js
@@ -14,7 +14,7 @@ const checkAuth = require('../middlewares/check-auth');
 router.get("/", UserController.get_user);
 
 
-router.post("/google", UserController.sign_google);
+router.post("/sign_google", UserController.sign_google);
 
 
 router.post("/signup", UserController.signup);
